b7bb8dafcf3c1a105bc6c7b72dfdc72a982c31a6
C2002G <claudiogabrieldev@gmail.com>
2025-10-03 14:19:49 -0300

    orm- inicio

--- Api com Flask/src/app.py
+++ Api com Flask/src/app.py
@@ -1,4 +1,4 @@
-import os
+
 
 from flask import Flask
 
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'diobank.sqlite'),
+        DATABASE='diobank.sqlite',
     )
 
     if test_config is None:
@@ -18,13 +18,14 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    
+    
 
     # a simple page that says hello
    
+
+
+    from . import db
+    db.init_app(app)
 
     return app
