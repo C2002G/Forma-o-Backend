2de8178f3cf29a7d6fb81218018510c4b0a45ad1
C2002G <claudiogabrieldev@gmail.com>
2025-10-10 15:47:14 -0300

    CRUD

--- Api com Flask/dio_bank/src/app.py
+++ Api com Flask/dio_bank/src/app.py
@@ -1,36 +1,72 @@
-from flask import Flask, url_for, request
+import os
+import click
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+import sqlalchemy as sa
+from datetime import datetime
 
-app = Flask(__name__)
-
-
-# rotas e endpoints
-@app.route("/")
-def home():
-    return "<p>Início</p>"
-
-
-@app.route("/olamundo")
-def hello_world():
-    return "<p>Hello, World!</p>"
+import click
 
 
-@app.route("/bemvindo/<usuario>")
-def bemvindo(usuario):
-    return f"<p>Bem vindo {usuario}</p>"
+class Base(DeclarativeBase):
+    pass
 
 
-@app.route("/projects/")
-def projects():
-    return "The project page"
+db = SQLAlchemy(model_class=Base)
+
+#ORM
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r})"
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str]= mapped_column(sa.String, nullable=False)
+    body: Mapped[str]= mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey("user.id"))
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
+    
 
 
-@app.route("/about", methods=['GET', 'POST'])
-def about():
-    return "The about page"
+
+@click.command("init-db")
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():  # current app para pegar a variavel app q esta local
+        db.create_all()
+    click.echo("Initialized the database.")
 
 
-with app.test_request_context():
-    print(url_for("hello_world"))
-    print(url_for("bemvindo", usuario="Skol"))
-    print(url_for("projects"))
-    print(url_for("about", next="/"))
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(
+        __name__, instance_relative_config=True
+    )  # vai procurar um arquivo de config
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        SQLALCHEMY_DATABASE_URI="sqlite:///dio_bank.sqlite",
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # register cli comands
+    app.cli.add_command(init_db_command)
+
+    # initialize extensions
+    db.init_app(app)
+
+    return app
