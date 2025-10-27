0fb9db518d4631085da70508d837025e6ad6e5df
C2002G <claudiogabrieldev@gmail.com>
2025-10-27 07:38:35 -0300

    desde a 5

--- Api com Flask/dio_bank/src/utils.py
+++ Api com Flask/dio_bank/src/utils.py
@@ -5,17 +5,21 @@ from src.app import User, db
 from functools import wraps
 
 
-def requires_role(rola_name):
+def requires_role(role_name):
     def decorator(f):
         @wraps(f)
         def wrapped(*args, **kwargs):
             user_id = get_jwt_identity()
             user = db.get_or_404(User, user_id)
 
-            if user.role.name != rola_name:
+            if user.role.name != role_name:
                 return {"message": "User dont have access."}, HTTPStatus.FORBIDDEN
             return f(*args, **kwargs)
 
-        return wrapped()
+        return wrapped
 
     return decorator
+
+
+def eleva_quadrado(x):
+    return x**2
