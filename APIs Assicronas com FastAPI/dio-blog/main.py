79942ce258d2debf83de47c2270f1ef3f840ba44
C2002G <claudiogabrieldev@gmail.com>
2025-11-11 15:46:31 -0300

    finalizado com query

--- APIs Assicronas com FastAPI/dio-blog/main.py
+++ APIs Assicronas com FastAPI/dio-blog/main.py
@@ -1,20 +1,60 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from datetime import datetime, UTC
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
 fake_db = [
-    {"title": "Criando uma aplicação com Django", "date": datetime.now(UTC)},
-    {"title": "Criando uma aplicação com Flask", "date": datetime.now(UTC)},
-    {"title": "Criando uma aplicação com FastAPI", "date": datetime.now(UTC)},
-    {"title": "Criando uma aplicação com scarlet", "date": datetime.now(UTC)},
+    {
+        "title": "Criando uma aplicação com Django",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": "Criando uma aplicação com Flask",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": "Criando uma aplicação com FastAPI",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": "Criando uma aplicação com scarlet",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
 ]
 
 
+# https://fastapi.tiangolo.com/tutorial/body/
+class Post(BaseModel):
+    name: str
+    date: datetime = datetime.now(UTC)
+    published: bool = False
+
+
+@app.post("/posts/", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    fake_db.append(post.model_dump())
+    return post
+
+
 @app.get("/posts")
-def read_posts(skip: int = 0, limit: int = len(fake_db)):
-    return fake_db[skip: skip + limit]
+def read_posts(published: bool, limit: int, skip: int = 0):
+    return [
+        post for post in fake_db[skip: skip + limit] if post["published"] is published
+    ]
+    # posts = []
+    # for post in fake_db:
+    #     if len(posts) == limit:
+    #         break
+    #     if post["published"] is published:
+    #         posts.append(post)
+    # return posts
+    # def read_posts(published: bool, skip: int = 0, limit: int = len(fake_db)):
 
 
 @app.get("/posts/{framework}")
